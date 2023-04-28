64fc66b668db7cfb21ac68f5616d3d4032c970f9
arulmathews117 <65379652+arulmathews117@users.noreply.github.com>
2023-04-28 09:38:03 +0530

    data storage and presentation

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,14 +2,16 @@ import { useEffect, useState } from "react";
 import { signOut, onAuthStateChanged } from "firebase/auth";
 import { auth } from "../firebaseConfig";
 import { useNavigate } from "react-router-dom";
-import { collection, addDoc } from "firebase/firestore";
-
+import { collection, addDoc, onSnapshot, doc } from "firebase/firestore";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function Home({ database }) {
   let databaseCollection = collection(database, "docs-data");
   let userEmail = localStorage.getItem("userEmail");
   const [isAdd, setIsAdd] = useState(false);
   const [title, setTitle] = useState("");
+  const [docsData, setDocsData] = useState([]);
   let navigate = useNavigate();
   const logout = () => {
     signOut(auth).then(() => {
@@ -29,22 +31,37 @@ export default function Home({ database }) {
 
   const addProject = () => {
     addDoc(databaseCollection, {
-        title: title,
-        author: userEmail,
-        body: ""
+      title: title,
+      author: userEmail,
+      body: "",
     })
-    .then(response => {
-        alert("Data Added")
+      .then((response) => {
+        toast.success("Project Created", {
+          autoClose: 1000,
+        });
         setIsAdd(false);
-        setTitle("")
-    })
-    .catch(() => {
-        alert("Cannot Add Data")
-    });
+        setTitle("");
+      })
+      .catch(() => {
+        toast.error("Cannot Add Data", {
+          autoClose: 1000,
+        });
+      });
   };
+
+  useEffect(() => {
+    onSnapshot(databaseCollection, (response) => {
+      setDocsData(
+        response.docs.map((doc) => {
+          return { ...doc.data(), id: doc.id };
+        })
+      );
+    });
+  }, []);
 
   return (
     <div>
+      <ToastContainer />
       <h1>Home</h1>
       <div className="logout-container">
         <button className="logout-btn" onClick={logout}>
@@ -52,7 +69,7 @@ export default function Home({ database }) {
         </button>
       </div>
 
-      <button onClick={() => setIsAdd(true)} className="add-doc">
+      <button onClick={() => setIsAdd(!isAdd)} className="add-doc">
         Add Project
       </button>
       {isAdd ? (
@@ -65,11 +82,23 @@ export default function Home({ database }) {
               setTitle(event.target.value);
             }}
           />
-          <button onClick={addProject} className="add-btn">Add</button>
+          <button onClick={addProject} className="add-btn">
+            Add
+          </button>
         </div>
       ) : (
         <></>
       )}
+
+      <div className="grid-main">
+        {docsData.map((doc) => {
+          return (
+            <div className="grid-child">
+              <h3>{doc.title}</h3>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
