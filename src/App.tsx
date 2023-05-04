3573b50193a3c1a459b36b193810c0c5b9c3178b
arulmathews117 <65379652+arulmathews117@users.noreply.github.com>
2023-05-04 11:17:41 +0530

    modified:   package-lock.json modified:   package.json modified:   src/App.tsx new file:   src/components/Auth.tsx modified:   src/components/Home.tsx modified:   src/index.css modified:   src/main.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Home from "./components/Home";
+import Auth from "./components/Auth";
 import { Routes, Route } from "react-router-dom";
 import { app, database } from "./firebaseConfig";
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <div>
       <Routes>
+        <Route path="/" element={<Auth />} />
         <Route path="/home/:id" element={<Home />} />
       </Routes>
     </div>
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,43 @@
+import React, { useState } from "react";
+import { FcOpenedFolder } from "react-icons/fc";
+import { FiFilePlus } from "react-icons/fi";
+import { Modal, Input } from "antd";
 
 export default function Home() {
-    return (<div>
-    </div>);
+    const [folderName, setFolderName] = useState("");
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+
+  return (
+    <div>
+      <div className="icon-container">
+        <div className="upload-btn-wrapper">
+          <FiFilePlus size={50} className="icon" color="#ffc107" />
+          <input type="file" name="myfile" />
+        </div>
+        
+        <FcOpenedFolder size={50} className="icon" onClick={showModal} />
+      </div>
+
+      <Modal
+        title="Folder Upload"
+        open={isModalOpen}
+        onOk={handleOk}
+        onCancel={handleCancel}
+        centered
+      >
+        <Input placeholder="File Name..." onChange={(event) => setFolderName(event.target.value)} />
+      </Modal>
+    </div>
+  );
 }
